962b89fe3d1dfe406e71337efdbe5d0357ee0d5c
gauravahlawat81 <gauravahlawat81@gmail.com>
2020-12-06 23:54:38 +0530

    Added

---
+++ scratch/scenario3.cc
@@ -0,0 +1,223 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+*   Copyright (c) 2011 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+*   Copyright (c) 2015, NYU WIRELESS, Tandon School of Engineering, New York University
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License version 2 as
+*   published by the Free Software Foundation;
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*   Author: Marco Miozzo <marco.miozzo@cttc.es>
+*           Nicola Baldo  <nbaldo@cttc.es>
+*
+*   Modified by: Marco Mezzavilla < mezzavilla@nyu.edu>
+*                         Sourjya Dutta <sdutta@nyu.edu>
+*                         Russell Ford <russell.ford@nyu.edu>
+*                         Menglei Zhang <menglei@nyu.edu>
+*/
+
+
+/* scenario3.cc
+ * Modified by: Artur André A. M. Oliveira <arturao@ime.usp.br>
+ * Based on the file src/mmwave/examples/mmwave-example.cc
+ * */
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/config-store.h"
+#include "ns3/mmwave-helper.h"
+#include <ns3/buildings-helper.h>
+#include "ns3/global-route-manager.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/log.h"
+#include "ns3/netanim-module.h"
+
+
+using namespace ns3;
+using namespace mmwave;
+
+void
+NotifyConnectionEstablishedUe (std::string context,
+                               uint64_t imsi,
+                               uint16_t cellid,
+                               uint16_t rnti)
+{
+  std::cout << context
+            << " UE IMSI " << imsi
+            << ": connected to CellId " << cellid
+            << " with RNTI " << rnti
+            << std::endl;
+}
+
+void
+NotifyHandoverStartUe (std::string context,
+                       uint64_t imsi,
+                       uint16_t cellid,
+                       uint16_t rnti,
+                       uint16_t targetCellId)
+{
+  std::cout << context
+            << " UE IMSI " << imsi
+            << ": previously connected to CellId " << cellid
+            << " with RNTI " << rnti
+            << ", doing handover to CellId " << targetCellId
+            << std::endl;
+}
+
+void
+NotifyHandoverEndOkUe (std::string context,
+                       uint64_t imsi,
+                       uint16_t cellid,
+                       uint16_t rnti)
+{
+  std::cout << context
+            << " UE IMSI " << imsi
+            << ": successful handover to CellId " << cellid
+            << " with RNTI " << rnti
+            << std::endl;
+}
+
+void
+NotifyConnectionEstablishedEnb (std::string context,
+                                uint64_t imsi,
+                                uint16_t cellid,
+                                uint16_t rnti)
+{
+  std::cout << context
+            << " eNB CellId " << cellid
+            << ": successful connection of UE with IMSI " << imsi
+            << " RNTI " << rnti
+            << std::endl;
+}
+
+void
+NotifyHandoverStartEnb (std::string context,
+                        uint64_t imsi,
+                        uint16_t cellid,
+                        uint16_t rnti,
+                        uint16_t targetCellId)
+{
+  std::cout << context
+            << " eNB CellId " << cellid
+            << ": start handover of UE with IMSI " << imsi
+            << " RNTI " << rnti
+            << " to CellId " << targetCellId
+            << std::endl;
+}
+
+void
+NotifyHandoverEndOkEnb (std::string context,
+                        uint64_t imsi,
+                        uint16_t cellid,
+                        uint16_t rnti)
+{
+  std::cout << context
+            << " eNB CellId " << cellid
+            << ": completed handover of UE with IMSI " << imsi
+            << " RNTI " << rnti
+            << std::endl;
+}
+
+
+int
+main (int argc, char *argv[])
+{
+
+  Config::SetDefault ("ns3::MmWavePhyMacCommon::ResourceBlockNum", UintegerValue (1));
+  Config::SetDefault ("ns3::MmWavePhyMacCommon::ChunkPerRB", UintegerValue (72));
+  Config::SetDefault ("ns3::MmWave3gppPropagationLossModel::Shadowing",
+		  BooleanValue(false)); // enable or disable the shadowing effect
+  CommandLine cmd;
+  cmd.Parse (argc, argv);
+
+  /* Information regarding the traces generated:
+   *
+   * 1. UE_1_SINR.txt : Gives the SINR for each sub-band
+   *    Subframe no.  | Slot No. | Sub-band  | SINR (db)
+   *
+   * 2. UE_1_Tb_size.txt : Allocated transport block size
+   *    Time (micro-sec)  |  Tb-size in bytes
+   * */
+
+  Ptr<MmWaveHelper> ptr_mmWave = CreateObject<MmWaveHelper> ();
+
+  ptr_mmWave->Initialize ();
+  ptr_mmWave->SetLteHandoverAlgorithmType("ns3::A2A4RsrqHandoverAlgorithm");
+
+  /* A configuration example. */
+  int ueNodesNum1 = 1;
+
+  int enbNodesNum = 2;
+  NodeContainer enbNodes;
+  NodeContainer ueNodes1;
+
+  enbNodes.Create(enbNodesNum);
+  ueNodes1.Create (ueNodesNum1);
+
+  Ptr<ListPositionAllocator> enbPositionAlloc = CreateObject<ListPositionAllocator> ();
+ 
+  enbPositionAlloc->Add (Vector (0,0,0));
+  enbPositionAlloc->Add (Vector (100,0,0));
+
+  MobilityHelper enbmobility;
+  enbmobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  enbmobility.SetPositionAllocator (enbPositionAlloc);
+  enbmobility.Install (enbNodes);
+  BuildingsHelper::Install (enbNodes);
+
+
+  MobilityHelper uemobility1;
+  uemobility1.SetMobilityModel ("ns3::ConstantVelocityMobilityModel");
+  uemobility1.Install (ueNodes1);
+
+  ueNodes1.Get (0)->GetObject<MobilityModel> ()->SetPosition (Vector (0, 10, 0));
+  ueNodes1.Get (0)->GetObject<ConstantVelocityMobilityModel> ()->SetVelocity (Vector (50, 0, 0));
+  
+
+  BuildingsHelper::Install (ueNodes1);
+
+  NetDeviceContainer enbNetDev = ptr_mmWave->InstallEnbDevice (enbNodes);
+  NetDeviceContainer ueNetDev1 = ptr_mmWave->InstallUeDevice (ueNodes1);
+
+  ptr_mmWave->AttachToClosestEnb (ueNetDev1, enbNetDev);
+
+  ptr_mmWave->EnableTraces ();
+
+  // Activate a data radio bearer
+  enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
+  EpsBearer bearer (q);
+  ptr_mmWave->ActivateDataRadioBearer (ueNetDev1, bearer);
+
+  // connect custom trace sinks for RRC connection establishment and handover notification
+  Config::Connect ("/NodeList/*/DeviceList/*/LteEnbRrc/ConnectionEstablished",
+                   MakeCallback (&NotifyConnectionEstablishedEnb));
+  Config::Connect ("/NodeList/*/DeviceList/*/LteUeRrc/ConnectionEstablished",
+                   MakeCallback (&NotifyConnectionEstablishedUe));
+  Config::Connect ("/NodeList/*/DeviceList/*/LteEnbRrc/HandoverStart",
+                   MakeCallback (&NotifyHandoverStartEnb));
+  Config::Connect ("/NodeList/*/DeviceList/*/LteUeRrc/HandoverStart",
+                   MakeCallback (&NotifyHandoverStartUe));
+  Config::Connect ("/NodeList/*/DeviceList/*/LteEnbRrc/HandoverEndOk",
+                   MakeCallback (&NotifyHandoverEndOkEnb));
+  Config::Connect ("/NodeList/*/DeviceList/*/LteUeRrc/HandoverEndOk",
+                   MakeCallback (&NotifyHandoverEndOkUe));
+
+  AnimationInterface anim ("scenario3.xml");
+
+  Simulator::Stop (Seconds (2));
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
+}
